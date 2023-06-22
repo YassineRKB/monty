8107bd71b4d3e327af24881cca389ae741d73e42
Yassine Rakibi <yassine@arcraven.com>
2023-06-22 23:53:49 +0200

    pint

--- monty.h
+++ monty.h
@@ -96,4 +96,5 @@ void invalidInstructionHandler(void);
 /* opcode funcs */
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 #endif
