18d2637eae0e285ff5c4e233fd834adef7370040
Yassine Rakibi <yassine@arcraven.com>
2023-06-23 02:01:10 +0200

    stack

---
+++ funcs_opcodeHandler4.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * stack -  LIFO opcode to sets the structure of data in stack.
+ * @stack: affected stack.
+ * @line_number: number of the executed line.
+ */
+void stack(stack_t **stack, unsigned int line_number)
+{
+	data->stack = 1;
+	(void) stack;
+	(void) line_number;
+}
